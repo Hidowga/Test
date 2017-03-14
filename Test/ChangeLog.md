01d469bd9becf7f08d06803c48b9bf2df47325ee
Gabriel A. Degenhardt <hidowga@gmail.com>
2017-03-14 15:21:16 -0300

    Haskell adicionado
    haskell testes

---
+++ Test/ChangeLog.md
@@ -0,0 +1,5 @@
+# Revision history for Test
+
+## 0.1  -- YYYY-mm-dd
+
+* First version. Released on an unsuspecting world.
---
+++ Test/Main.hs
@@ -0,0 +1,4 @@
+module Main where
+import           Person
+main :: IO ()
+main = print$firstName$Person  "Gabriel" "Ariel" 17 1.90 "3330-7320" "morango"
